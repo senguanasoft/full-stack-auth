4078b3386691c98f4a3a23a2d48835228bbf78b1
emiliosdev <sengua2000@outlook.com>
2025-08-08 11:46:59 -0500

    add swagger

--- auth-backend/src/main.ts
+++ auth-backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Agrega esta línea para establecer el prefijo global
@@ -17,6 +18,17 @@ async function bootstrap() {
 
   // Habilita los pipes de validación globalmente
   app.useGlobalPipes(new ValidationPipe());
+
+  // Swagger documentation
+  const config = new DocumentBuilder()
+    .setTitle('Authentication API')
+    .setDescription('API documentation for authentication system')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
